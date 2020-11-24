27bcd3f5541b5176ae99c4e333f8d8927e1c3aa2
root <root@localhost.localdomain>
2020-11-24 18:05:04 +0530

    second

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
-print("Hello")
-print("Hi")
+#!/usr/bin/python3
+
+# importing files
+from docker import docker_script
+from aws import aws_script
+from ansible import ansible_script
+from hadoop import hadoop_script
+from linux import linux_script
+
+import os
+
+msg = "Ninja3"
+print(os.system("figlet {}".format(msg)))
+
